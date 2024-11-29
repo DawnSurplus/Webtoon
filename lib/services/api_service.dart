44715b4efacbf5fb1da3d990ac56fddc2c2c4bc5
Dawn Surplus <>
2024-11-29 17:37:47 +0900

    [resolve #4] webtoon model class 생성 및 API로부터 받은 데이터 저장

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,17 +1,28 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:webtoon/models/webtoon_model.dart';
 
 class APIService {
   final String baseURL = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String todyay = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+
     final url = Uri.parse("$baseURL/$todyay");
     final response = await http.get(url);
 
     if (response.statusCode == HttpStatus.ok) {
-      print(response.body);
-    } else {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final toon = WebtoonModel.formJson(webtoon);
+        webtoonInstances.add(toon);
+      }
+
+      return webtoonInstances;
+    }
+    else {
       throw Error();
     }
   }
