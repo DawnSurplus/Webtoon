b01b293f58b1cf60a92d8f77cd83af890679a7a7
Dawn Surplus <>
2024-12-01 17:44:03 +0900

    [resolve #16] 좋아요 기능 적용

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:webtoon/models/webtoon_detail_model.dart';
 import 'package:webtoon/models/webtoon_episode_model.dart';
 import 'package:webtoon/models/webtoon_model.dart';
@@ -17,12 +18,45 @@ class DetailScreen extends StatefulWidget {
 class _DetailScreenState extends State<DetailScreen> {
   late Future<WebtoonDetailModel> webtoon;
   late Future<List<WebtoonEpisodeModel>> episode;
+  late SharedPreferences prefs;
+  bool isLiked = false;
 
   @override
   void initState() {
     super.initState();
     webtoon = APIService.getToonById(widget.webtoon.id);
     episode = APIService.getLatestEpisodesById(widget.webtoon.id);
+    initPrefs();
+  }
+
+  Future initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (likedToons.contains(widget.webtoon.id)) {
+        setState(() {
+          isLiked = true;
+        });
+      }
+    } else {
+      prefs.setStringList('likedToons', []);
+    }
+  }
+
+  onHeartTap() async {
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.webtoon.id);
+      } else {
+        likedToons.add(widget.webtoon.id);
+      }
+
+      await prefs.setStringList('likedToons', likedToons);
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
   }
 
   @override
@@ -35,6 +69,12 @@ class _DetailScreenState extends State<DetailScreen> {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: onHeartTap,
+              icon: Icon(
+                  isLiked ? Icons.favorite : Icons.favorite_outline_outlined))
+        ],
         title: Text(
           widget.webtoon.title,
           style: const TextStyle(fontSize: 24),
